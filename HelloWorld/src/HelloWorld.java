b75e8c185bc8d95f44669eaf64f44e7e8c7c668f
selfconfig <kyulin7.kwon@samsung.com>
2018-01-22 00:52:05 +0900

    master branch change acode

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("We gonna party tonight!!");
 		//이클스와 깃헙 오예 
+	System.out.println("Party goes on forever");
 
 	}
 
