8bf1d401f5115854cf109c1ac53b0460af944528
selfconfig <kyulin7.kwon@samsung.com>
2018-01-22 23:15:13 +0900

    PushPrint.java modified on GitHub

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -4,6 +4,8 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add java class file path");
+		System.out.println("World with 42");
+
 
 	}
 
